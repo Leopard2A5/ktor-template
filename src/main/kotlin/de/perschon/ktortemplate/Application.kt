32c77ae1906f18ecd814db842f181aa52e87d0d2
René Perschon <rperschon85@gmail.com>
2019-04-14 18:59:20 +0200

    load config correctly with overrides

--- src/main/kotlin/de/perschon/ktortemplate/Application.kt
+++ src/main/kotlin/de/perschon/ktortemplate/Application.kt
@@ -1,23 +1,20 @@
 package de.perschon.ktortemplate
 
-import com.uchuhimo.konf.Config
 import de.perschon.ktortemplate.config.KonfKtorConfig
 import de.perschon.ktortemplate.config.Ktor
+import de.perschon.ktortemplate.config.loadConfig
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.applicationEngineEnvironment
-import io.ktor.server.engine.commandLineEnvironment
 import io.ktor.server.engine.connector
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun main() {
-    val config = Config { addSpec(Ktor) }
-        .from.hocon.resource("application.conf")
-        .from.env()
+    val config = loadConfig()
 
     val env = applicationEngineEnvironment {
         this.config = KonfKtorConfig(config = config)
--- src/main/kotlin/de/perschon/ktortemplate/config/Config.kt
+++ src/main/kotlin/de/perschon/ktortemplate/config/Config.kt
@@ -1,6 +1,11 @@
 package de.perschon.ktortemplate.config
 
+import com.uchuhimo.konf.Config
 import com.uchuhimo.konf.ConfigSpec
+
+val topLevelConfigs = arrayOf(
+    Ktor
+)
 
 object Ktor : ConfigSpec() {
     object Application : ConfigSpec() {
@@ -10,4 +15,19 @@ object Ktor : ConfigSpec() {
         val port by required<Int>()
         val host by optional("0.0.0.0")
     }
+}
+
+fun loadConfig(): Config {
+    val env = System.getenv("ENV") ?: "development"
+
+    val conf = Config {
+        topLevelConfigs.forEach { addSpec(it) }
+    }.from.hocon.resource("application.conf")
+
+    return when(env) {
+        "development" -> conf
+        "testing" -> conf.from.hocon.resource("application-testing.conf")
+        "production" -> conf.from.hocon.resource("application-testing.conf")
+        else -> throw IllegalArgumentException("Unsupported environment: $env")
+    }.from.env()
 }
